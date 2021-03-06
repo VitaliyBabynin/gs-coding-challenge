d1f08f183bce8c2946cdf764169bfba660d0452b
Vitalii <vitaliy_babynin@hotmail.com>
2021-03-06 16:39:02 +0100

    Email validation added

--- src/buyflow/EmailStep.tsx
+++ src/buyflow/EmailStep.tsx
@@ -6,12 +6,30 @@ interface EmailStepProps {
 
 const EmailStep: React.FC<EmailStepProps> = (props) => {
     const [email, setEmail] = useState('');
+    const [warningMessage, setWarningMessage] = useState('');
+    const fieldName = 'Email';
+
     return <>
-        <div>Email: <input type='email' onChange={({target: {value}}) => {setEmail(value)}} value={email}></input></div>
+        <div className={'warning'}>{warningMessage}</div>
+        <div>{fieldName}: <input type='email' onChange={({target: {value}}) => {setEmail(value)}} value={email}></input></div>
         <button onClick={() => onClickNext()}>Next</button>
     </>;
+
     function onClickNext(){
+        if (!isEmailValid()) return;
         props.cb('email', email)
+    }
+
+    function isEmailValid() {
+        if (email.trim() === '') {
+            setWarningMessage(`${fieldName} is required`);
+            return false;
+        }
+        if (!/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email,)) {
+            setWarningMessage(`Please enter a valid email`);
+            return false;
+        }
+        return true;
     }
 };
 
