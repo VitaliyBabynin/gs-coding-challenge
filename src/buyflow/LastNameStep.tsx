a9ed24ebfd8a821b7c77a5097935845a6d1aadbb
Vitalii <vitaliy_babynin@hotmail.com>
2021-03-06 16:25:41 +0100

    Last Name validation implemented

--- src/buyflow/LastNameStep.tsx
+++ src/buyflow/LastNameStep.tsx
@@ -6,12 +6,34 @@ interface LastNameStepProps {
 
 const LastNameStep: React.FC<LastNameStepProps> = (props) => {
     const [lastName, setLastName] = useState('');
+    const [warningMessage, setWarningMessage] = useState('');
+    const fieldName = 'Last Name';
+
     return <>
-        <div>Last Name: <input type='string' onChange={({target: {value}}) => {setLastName(value)}} value={lastName}></input></div>
+        <div className={'warning'}>{warningMessage}</div>
+        <div>{fieldName}: <input type='string' onChange={({target: {value}}) => {setLastName(value)}} value={lastName}></input></div>
         <button onClick={() => onClickNext()}>Next</button>
     </>;
+
     function onClickNext(){
+        if (!isLastNameValid()) return;
         props.cb('lastName', lastName)
+    }
+
+    function isLastNameValid(){
+        if (lastName.trim() === '') {
+            setWarningMessage(`${fieldName} is required`);
+            return false;
+        }
+        if (/[^a-zA-Z -]/.test(lastName)) {
+            setWarningMessage(`Invalid characters`);
+            return false;
+        }
+        if (lastName.trim().length < 3) {
+            setWarningMessage(`${fieldName} needs to be at least three characters`);
+            return false;
+        }
+        return true;
     }
 };
 
