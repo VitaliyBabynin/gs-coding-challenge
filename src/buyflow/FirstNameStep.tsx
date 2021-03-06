1efc4bd49782c19eb85a83adcb8487c13c2de354
Vitalii <vitaliy_babynin@hotmail.com>
2021-03-06 16:19:08 +0100

    First Name validation implemented

--- src/buyflow/FirstNameStep.tsx
+++ src/buyflow/FirstNameStep.tsx
@@ -6,12 +6,34 @@ interface FirstNameStepProps {
 
 const FirstNameStep: React.FC<FirstNameStepProps> = (props) => {
     const [firstName, setFirstName] = useState('');
+    const [warningMessage, setWarningMessage] = useState('');
+    const fieldName = 'First Name';
+
     return <>
-        <div>First Name: <input type='string' onChange={({target: {value}}) => {setFirstName(value)}} value={firstName}></input></div>
+        <div className={'warning'}>{warningMessage}</div>
+        <div>{fieldName}: <input type='string' onChange={({target: {value}}) => {setFirstName(value)}} value={firstName}></input></div>
         <button onClick={() => onClickNext()}>Next</button>
     </>;
+
     function onClickNext(){
+        if (!isFirstNameValid()) return;
         props.cb('firstName', firstName)
+    }
+
+    function isFirstNameValid(){
+        if (firstName.trim() === '') {
+            setWarningMessage(`${fieldName} is required`);
+            return false;
+        }
+        if (/[^a-zA-Z -]/.test(firstName)) {
+            setWarningMessage(`Invalid characters`);
+            return false;
+        }
+        if (firstName.trim().length < 3) {
+            setWarningMessage(`${fieldName} needs to be at least three characters`);
+            return false;
+        }
+        return true;
     }
 };
 
